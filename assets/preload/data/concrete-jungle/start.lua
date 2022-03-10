6de4b5f22c7caf4b51fc0a0aa6c7a277129261b5
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-03-10 07:44:56 -0300

    Add files via upload

---
+++ assets/preload/data/concrete-jungle/start.lua
@@ -0,0 +1,28 @@
+local allowCountdown = false
+local startedFirstDialogue = false
+
+function onStartCountdown()
+    if not allowCountdown and isStoryMode and not startedFirstDialogue then
+        setProperty('inCutscene', true);
+        runTimer('animation', 0.5);
+        runTimer('startDialogue', 2.5);
+        startedFirstDialogue = true;
+        return Function_Stop;
+    end
+    return Function_Continue;
+end
+
+function onTimerCompleted(tag, loops, loopsLeft)
+    if tag == 'startDialogue' then
+        startDialogue('Dialogue1', 'dialogueAmbience1');
+    end
+
+    if tag == 'animation' then
+        triggerEvent('Play Animation', 'intro', 'dad')
+        playSound('aceanim', 2)
+    end
+end
+
+function onCreate()
+    precacheSound('aceanim')
+end
